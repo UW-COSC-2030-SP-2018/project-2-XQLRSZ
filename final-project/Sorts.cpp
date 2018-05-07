17ae5d55793c294ff607e7c7a391bdef8c5a08fb
Aaron J McClellan <amcclel2@uwyo.edu>
2018-05-07 16:21:18 -0600

    Nothing passed the tests. Time to redo it

--- final-project/Sorts.cpp
+++ final-project/Sorts.cpp
@@ -11,7 +11,7 @@ void Sort::quicksort(int arr[], int low, int high) {
 int Sort::partition(int arr[], int low, int high) {
 	int pivot = arr[high];
 	int i = low - 1;
-	for (int j = low; j < high - 1; j++) {
+	for (int j = low; j <= high; j++) {
 		if (arr[j] < pivot) {
 			i++;
 			swap(arr, i, j);
@@ -28,47 +28,44 @@ void Sort::swap(int arr[], int a, int b) {
 }
 
 void Sort::mergesort(int arr[], int low, int high) {
-	if (high > low) {
-		int mid = (low + high) / 2;
-		mergesort(arr, low, mid);
-		mergesort(arr, mid + 1, high);
-		merge(arr, low, mid, high);
+	if (low - high < 2) {
+		return;
 	}
+	int mid = (low + high) / 2;
+	mergesort(arr, low, mid);
+	mergesort(arr, mid, high);
+	merge(arr, low, mid, high);
 }
 
 void Sort::merge(int arr[], int low, int mid, int high) {
-	int size1 = mid - low + 1;
-	int size2 = high - mid;
-	int i = size1, j = size2, k = low;
-	while (i < size1 && j < size2) {
-		if (arr[i] <= arr[j]) {
+	int i = low, j = high;
+	for (int k = low; k < high; k++) {
+		if (i < mid && (j >= high || arr[i] <= arr[j])) {
 			arr[k] = arr[i];
 			i++;
-		}
-		else {
+		} else {
 			arr[k] = arr[j];
 			j++;
 		}
-		k++;
 	}
 }
 
 int Sort::bsearch(int arr[], int size, int key) {
-	bsearch(arr, 0, size - 1, key);
+	return bsearch(arr, 0, size - 1, key);
 }
 
 int Sort::bsearch(int arr[], int low, int high, int key) {
 	int mid = (high + low) / 2;
 
-	if (mid < key) {
+	if (arr[mid] == key) {
+		return mid;
+	} else if (mid == low || mid == high) {
+		return -1;
+	}
+
+	if (arr[mid] < key) {
 		return bsearch(arr, low, mid, key);
 	} else {
 		return bsearch(arr, mid + 1, high, key);
-	}
-
-	if (mid == key) {
-		return mid;
-	} else if (mid == 0) {
-		return -1;
 	}
 }
