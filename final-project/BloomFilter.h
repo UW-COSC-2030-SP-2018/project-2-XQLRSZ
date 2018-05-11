eb0e744ef2f0b75fc7184d4dd5bcaf710aa4ee70
Aaron J McClellan <amcclel2@uwyo.edu>
2018-05-10 18:37:36 -0600

    It didn't

--- final-project/BloomFilter.h
+++ final-project/BloomFilter.h
@@ -1,24 +1,66 @@
+// This header is a mess because of this:
+// https://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file
+// Because of template stuff that I don't really understand,
+// templates must be defined in the header or use some other, non-ideal workarounds.
+// Can you help me understand this?
+
 #ifndef BLOOMFILTER_H
 #define BLOOMFILTER_H
 
 #include <bitset>
 #include <string>
-#include <functional>
 
 using namespace std;
 
 template<size_t SIZE, size_t N>
 class BloomFilter {
 public:
-	BloomFilter();
-	bitset getBits() const;
-	size_t getNumHashes() const;
-	void insert(string data);
-	bool contains(string data) const;
+
+	BloomFilter() : numHashes(N) {
+		if (SIZE < 1) {
+			throw "Size of Bloom filter must be greater than 0";
+		}
+		if (N < 1) {
+			throw "Number of hashes must be greater than 0";
+		}
+	}
+
+	bitset<SIZE> getBits() const {
+		return bits;
+	}
+
+	size_t getNumHashes() const {
+		return numHashes;
+	}
+
+	void insert(string data) {
+		for (size_t i = 0; i < numHashes; i++) {
+			bits[BloomFilter::hash(data, i) % SIZE] = 1;
+		}
+	}
+
+	bool contains(string data) const {
+		for (size_t i = 0; i < numHashes; i++) {
+			if (bits[BloomFilter::hash(data, i) % SIZE] == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 protected:
-	size_t hash(string data, size_t n) const;
+	size_t hash(string data, size_t n) const {
+		std::hash<string> hashFunc;
+		size_t hashVal = hashFunc(data);
+		for (size_t i = 1; i < n; i++) {
+			hashVal = hashFunc(to_string(hashVal));
+		}
+		return hashVal;
+	}
+
 private:
 	bitset<SIZE> bits;
 	const size_t numHashes;
 };
+
 #endif
